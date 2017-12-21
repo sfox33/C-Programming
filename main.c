b57738102168dd829e0d1a6410166d9e430438de
sfox33 <sfox33@uncc.edu>
2017-12-21 00:09:30 -0500

    Lab 1

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[])
+{
+    int firstInteger = 7;
+    float firstFloat = 9.0;
+
+    printf("The value of the first integer is %i", firstInteger);
+    printf("\nThe value of the first float is %.1f", firstFloat + 0.3);
+
+    return 0;
+}
+
